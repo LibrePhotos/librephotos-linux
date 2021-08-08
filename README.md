5596e17c7a123363e46a5e8a2103bbc1e9a42a14
Seneliux <47281748+Seneliux@users.noreply.github.com>
2021-08-08 11:59:03 +0200

    Installation splited to separate parts: backend, fronted, proxy

--- README.md
+++ README.md
@@ -5,66 +5,25 @@
 
 ## Pre-Installation
 
-### requirement 
-  - postgresql
-  - redis
-
-### for local postgresql and redis
-
-Install postgresql and redis
-
+Install git:
 ~~~
-apt install postgresql redis
-~~~
-
-### Postgresql database creation script
-
-Open sql console
-~~~
-su - postgres -c /usr/bin/psql
-~~~
-
-Execute the following script, changing values like password and username (you will input these on the config .env file)
-
-~~~
-CREATE USER librephotos WITH PASSWORD 'password';
-CREATE DATABASE "librephotos" WITH OWNER "librephotos" WITH TEMPLATE = template0 ENCODING = "UTF8";
-GRANT ALL privileges ON DATABASE librephotos TO librephotos;
-quit
+sudo apt install git -y
 ~~~
 
 ## Installation
 
 ### Debian like distribution
 
-Execute the following command as root
+Execute the following commands as root. This will create systemuser 'librephotos', creates directories, install necessary software, creates database and automaGically writes some variables to librephotos-backend.env file.
 ~~~
+sudo su
 cd /tmp/
-git clone https://github.com/LibrePhotos/librephotos-linux.git
+git clone https://github.com/Seneliux/librephotos-linux.git
 cd librephotos-linux
-./install-librephotos.sh 
+./install-librephotos_backend.sh
 ~~~
 
-An alternative would be to directly download the archive
-~~~
-wget https://github.com/librephotos/librephotos-linux/archive/main.zip -O /tmp/main.zip
-unzip -d /tmp/ /tmp/main.zip
-cd /tmp/librephotos-linux-main/
-./install-librephotos.sh
-~~~
-
-Edit /etc/librephotos/librephotos-backend.env to store configuration variables, such as:
-
- - SECRET_KEY
-
- - Postgresql information:
-~~~
-DB_HOST=localhost
-DB_PORT=5432
-DB_NAME=YOURDBNAME
-DB_USER=YOURUSER
-DB_PASS=YOURPASSWORD
-~~~
+Edit '/etc/librephotos/librephotos-backend.env' to store configuration variables, such as:
 
  - redis information
 In case you configured it with a password or are using a special path.
@@ -94,40 +53,13 @@ systemctl start librephotos-backend
 systemctl start librephotos-frontend
 ~~~
 
-### Other distribution
-
-not working yet
-
 ## additional information
 
 ### librephotos-cli
 
-As root you can use 
+As root you can use
 
 ~~~
 librephotos-cli build_similarity_index
 librephotos-cli clear_cache
-~~~
-
-### Samba mount point example
-
-Install cifs-utils :
-
-~~~
-apt install cifs-utils
-~~~
-
-In /etc/fstab add the following line :
-
-~~~
-
-//data.lan.lgy.fr/ftcl/photos/ /var/lib/librephotos/data/photos cifs uid=librephotos,gid=librephotos,credentials=/etc/samba/smbcredentials,iocharset=utf8,file_mode=0777,dir_mode=0777,sec=ntlmssp,noacl 0 0
-~~~
-
-create file /etc/samba/smbcredentials with connection information like the following
-
-~~~
-username=thomas
-password=mypassword
-domain=my.domain.com
 ~~~
---
+++ install-librephotos_backend.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+set -ea
+
+# PRE INSTALL
+id -g librephotos > /dev/null || groupadd -r librephotos
+id -u librephotos > /dev/null || useradd --home-dir /usr/lib/librephotos --comment "librephotos user" -g librephotos -mr -s /usr/sbin/nologin librephotos
+
+export BASE_DATA=/foto
+export BASE_LOGS=/var/log/librephotos
+
+
+mkdir -p $BASE_LOGS
+mkdir -p $BASE_DATA/data_models/{places365,im2txt}
+mkdir -p $BASE_DATA/protected_media/{thumbnails_big,square_thumbnails,square_thumbnails_small,faces}
+mkdir -p $BASE_DATA/data/nextcloud_media
+chown -R librephotos:librephotos $BASE_LOGS
+chown -R librephotos:librephotos $BASE_DATA
+
+# LIBREPHOTOS : BACKEND
+
+REQUIRED_PKG=( swig ffmpeg libimage-exiftool-perl libpq-dev postgresql-contrib curl libopenblas-dev libmagic1 libboost-all-dev libxrender-dev \
+liblapack-dev git bzip2 cmake build-essential libsm6 libglib2.0-0 libgl1-mesa-glx gfortran gunicorn \
+libheif-dev libssl-dev rustc liblzma-dev python3 python3-pip imagemagick redis )
+[ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ] && apt install --no-install-recommends -y $i > /dev/null 2>&1
+
+su - -s $(which bash) librephotos << EOF
+curl -SL https://github.com/LibrePhotos/librephotos-docker/releases/download/0.1/places365.tar.gz | tar -zxC $BASE_DATA/data_models/
+curl -SL https://github.com/LibrePhotos/librephotos-docker/releases/download/0.1/im2txt.tar.gz | tar -zxC $BASE_DATA/data_models/
+mkdir -p ~/.cache/torch/hub/checkpoints/
+curl -SL https://download.pytorch.org/models/resnet152-b121ed2d.pth -o ~/.cache/torch/hub/checkpoints/resnet152-b121ed2d.pth
+
+pip3 install torch==1.7.1+cpu torchvision==0.8.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install -v --install-option="--no" --install-option="DLIB_USE_CUDA" dlib
+
+https://github.com/Seneliux/librephotos.git backend
+cd backend
+pip3 install -r requirements.txt
+python3 -m spacy download en_core_web_sm
+EOF
+
+# CREATING DATABASE
+su - postgres << EOF
+psql -c 'CREATE USER librephotos;'
+psql -c 'CREATE DATABASE "librephotos" WITH OWNER "librephotos" TEMPLATE = template0 ENCODING = "UTF8";'
+psql -c 'GRANT ALL privileges ON DATABASE librephotos TO librephotos;'
+exit
+EOF
+echo 'su - postgres -c "psql -U postgres -d postgres -c \"alter user librephotos with password tmp_password;\""' > /tmp/database_pass
+pass=$( < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-12};)
+sed -i "s|tmp_password|'${pass}'|g" /tmp/database_pass
+chmod +x /tmp/database_pass
+/tmp/database_pass
+
+# POST INSTALL
+
+[ -d /usr/lib/librephotos/bin ] || mkdir -p /usr/lib/librephotos/bin
+cp ressources/bin/* /usr/lib/librephotos/bin/
+ln -fs /usr/lib/librephotos/bin/librephotos-cli /usr/sbin/librephotos-cli
+chown -R librephotos:librephotos  /usr/lib/librephotos/bin/
+cp -r ressources/etc/librephotos/ /etc/
+cp ressources/systemd/* /etc/systemd/system/
+sed -i "s|DB_PASS=password|DB_PASS=${pass}|g" /etc/librephotos/librephotos-backend.env
+secret_key=$( < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};)
+sed -i "s|SECRET_KEY=SecretKeyToBeDefined|SECRET_KEY=${secret_key}|g" /etc/librephotos/librephotos-backend.env
+rm /tmp/database_pass
+
+systemctl enable librephotos-backend
+systemctl enable librephotos-worker.service
+systemctl enable librephotos-image-similarity.service
