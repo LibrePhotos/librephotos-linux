74b8204ac4a3229046301440424d1b6b655dac8d
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-04-19 11:30:54 +0200

    Create entrypoint.sh

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+systemctl start librephotos-worker.service && systemctl start librephotos-backend && systemctl start librephotos-image-similarity.service && echo
+systemctl enable librephotos-backend
+systemctl enable librephotos-worker.service
+systemctl enable librephotos-image-similarity.service
+systemctl start librephotos-frontend
+systemctl enable librephotos-frontend
+pg_ctlcluster 13 main start
+systemctl start postgresql.service
+systemctl enable postgresql.service
+/usr/lib/librephotos/bin/librephotos-upgrade
