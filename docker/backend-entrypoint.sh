3290ee6c2319b8ebdd91e8e64d7d3dc972cf69d0
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-04-26 13:50:01 +0200

    Update backend-entrypoint.sh

--- docker/backend-entrypoint.sh
+++ docker/backend-entrypoint.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-cd /usr/lib/librephotos/backend
-if [ -n "$SECRET_KEY" ]
-then
-    echo "Use env SECRET_KEY"
-else 
-    if [ -f $BASE_LOGS/secret.key ]
-    then
-        echo "Use existing secret.key"
-        SECRET_KEY=`cat $BASE_LOGS/secret.key`
-        export SECRET_KEY=$SECRET_KEY
-    else
-        echo "Create new secret.key"
-        SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
-        echo $SECRET_KEY > $BASE_LOGS/secret.key
-        export SECRET_KEY=$SECRET_KEY
-    fi
-fi
-
 set -o allexport
 source /etc/librephotos/librephotos-backend.env
 set +o allexport
-
-cd /usr/lib/librephotos/backend
-python3 manage.py showmigrations 
-python3 manage.py migrate 
-python3 manage.py showmigrations
-python3 manage.py clear_cache 
-echo "Running production backend server..."
