64dc6b48d5094ddc0654ffcb9d554f70976b7536
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-04-27 16:08:48 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,8 +7,9 @@ systemctl enable librephotos-worker.service
 systemctl enable librephotos-image-similarity.service
 systemctl start librephotos-frontend
 systemctl enable librephotos-frontend
+export PATH=/lib/postgresql/13/bin:$PATH
 service postgresql start
-bash ./postgres-entrypoint.sh
+bash ./postgres-entrypoint.sh --name librephotos -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 systemctl start postgresql.service
 systemctl enable postgresql.service
 systemctl restart nginx
