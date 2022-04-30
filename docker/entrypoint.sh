e7c8598fd02790fb602f79b9b68ec0504fde53fb
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-04-30 13:56:06 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,6 @@ systemctl enable librephotos-image-similarity.service
 systemctl start librephotos-frontend
 systemctl enable librephotos-frontend
 export PATH=/lib/postgresql/13/bin:$PATH
-service postgresql start
 postgres-entrypoint.sh postgres
 systemctl start postgresql.service
 systemctl enable postgresql.service
