0975a4eb729b5b25b99ae0c32a51614bbf22a0bd
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-04-30 12:34:44 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,7 @@ systemctl start librephotos-frontend
 systemctl enable librephotos-frontend
 export PATH=/lib/postgresql/13/bin:$PATH
 service postgresql start
-postgres-entrypoint.sh --name $POSTGRES_DB -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -e POSTGRES_USER=$POSTGRES_USER
+postgres-entrypoint.sh postgres
 systemctl start postgresql.service
 systemctl enable postgresql.service
 systemctl restart nginx
