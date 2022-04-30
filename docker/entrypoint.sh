cd809e596668f2a5cfff04d84c01dd3f2f970bf3
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-04-30 12:02:16 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 redis-server --daemonize yes
-systemctl start librephotos-worker.service && systemctl start librephotos-backend && systemctl start librephotos-image-similarity.service && echo
+systemctl start librephotos-backend 
+systemctl start librephotos-image-similarity.service
 systemctl enable librephotos-backend
-systemctl enable librephotos-worker.service
 systemctl enable librephotos-image-similarity.service
 systemctl start librephotos-frontend
 systemctl enable librephotos-frontend
