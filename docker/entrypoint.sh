c39d9844936ea21be65776c3106e452330e9fb0f
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-05-06 23:53:06 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [[ -z "${FIXPERMISSIONS}" ]]; 
+then
+  echo "If you have file permission issues, use -e FIXPERMISSIONS=true"
+else
+  chmod -R 777 /var/lib/librephotos/photos/
+fi
+
 chown -R librephotos:librephotos /var/log/librephotos
-chown -R librephotos:librephotos /var/lib/librephotos/photos/
 chown -R librephotos:librephotos /var/lib/librephotos/data/protected_media/
+
 redis-server --daemonize yes
 systemctl restart librephotos-backend 
 systemctl restart librephotos-worker.service
