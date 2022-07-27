448669efee76469db3c5a983c781d560d9569c65
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-07-28 00:55:50 +0200

    Update install-librephotos.sh

--- install-librephotos.sh
+++ install-librephotos.sh
@@ -57,7 +57,7 @@ chown -R librephotos:librephotos $BASE_DATA
 
 # Add PPA to install older postgresql version
 # Create the file repository configuration:
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 # Import the repository signing key:
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 # Update the package lists:
