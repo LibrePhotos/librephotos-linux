ac2ae3c3e10b2967cf93679ec3beed3985fe7138
Niaz Faridani-Rad <tigrexspalterlp@gmail.com>
2022-07-28 13:46:26 +0200

    Update install-librephotos.sh

--- install-librephotos.sh
+++ install-librephotos.sh
@@ -63,7 +63,7 @@ done
 # Create the file repository configuration:
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 # Import the repository signing key:
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+wget --no-check-certificate --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 # Update the package lists:
 apt-get update
 
